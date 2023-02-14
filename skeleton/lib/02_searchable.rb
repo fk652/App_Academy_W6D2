317d04351695af3f80ca216327b19240c70716c2
Fahim Khan <fk652@nyu.edu>
2023-02-14 17:08:46 -0500

    finish part 2

--- skeleton/lib/02_searchable.rb
+++ skeleton/lib/02_searchable.rb
@@ -3,10 +3,18 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map{ |col| "#{col} = ?" }.join(" AND ")
+
+    query = DBConnection.execute(<<-SQL, *params.values)
+      SELECT *
+      FROM #{self.table_name}
+      WHERE #{where_line}
+    SQL
+
+    self.parse_all(query)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
