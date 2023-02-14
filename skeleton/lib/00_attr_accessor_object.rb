e3ce609f96097751ab27694a58f929cfac9a8b7a
Fahim Khan <fk652@nyu.edu>
2023-02-14 14:43:29 -0500

    finish part 00

--- skeleton/lib/00_attr_accessor_object.rb
+++ skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,18 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      var_name = name.to_s
+
+      # define a setter
+      define_method(var_name) do 
+        self.instance_variable_get("@#{var_name}")
+      end
+
+      #define a getter
+      getter_name = "#{var_name}="
+      define_method(getter_name) do |new_val|
+        self.instance_variable_set("@#{var_name}", new_val)
+      end
+    end
   end
 end
